573891210a6a8607201f4cea8380f9c65dfa2723
0CHOPA0 <67321394+0CHOPA0@users.noreply.github.com>
2025-03-07 14:36:25 +0900

    Update data.js

--- data.js
+++ data.js
@@ -1,3 +1,3 @@
-const employees = [
+const CertificateData = [
     { Cname: "DAEBO MAGNETIC CO.,LTD.", Addr: "25_13, Hyundaikiaro, Paltanmyeon, Hwaseongsi, Gyeonggido, Republic of Korea", AppF: "", Ccode: "082-03-026-01424", Std: "Standard Certificate of Compliance", Cstat: "Valid", Cdate: "04-12-2029" }
 ];
