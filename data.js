933a8d18e45edce661f3d61b87396d2a19aab9aa
0CHOPA0 <67321394+0CHOPA0@users.noreply.github.com>
2025-03-07 13:30:26 +0900

    Create data.js

---
+++ data.js
@@ -0,0 +1,7 @@
+const employees = [
+    { name: "김철수", id: "10001", email: "chulsoo@example.com" },
+    { name: "이영희", id: "10002", email: "younghee@example.com" },
+    { name: "박민수", id: "10003", email: "minsoo@example.com" },
+    { name: "최지은", id: "10004", email: "jieun@example.com" },
+    { name: "홍길동", id: "10005", email: "gildong@example.com" }
+];
