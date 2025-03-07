25b2970808eda62fe3ec10f4a8fe7d02b999b8d4
0CHOPA0 <67321394+0CHOPA0@users.noreply.github.com>
2025-03-07 13:34:53 +0900

    Update data.js

--- data.js
+++ data.js
@@ -3,6 +3,6 @@ const employees = [
     { name: "이영희", id: "10002", email: "younghee@example.com" },
     { name: "박민수", id: "10003", email: "minsoo@example.com" },
     { name: "최지은", id: "10004", email: "jieun@example.com" },
-    { name: "홍길동", id: "10005", email: "gildong@example.com" }
+    { name: "홍길동", id: "10005", email: "gildong@example.com" },
     { name: "박기동", id: "15410", email: "gidongg@example.com" }    
 ];
