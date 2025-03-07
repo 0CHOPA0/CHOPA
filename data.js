8fcbeb7fb91e42634df93dda92e2cc862a97bdb5
0CHOPA0 <67321394+0CHOPA0@users.noreply.github.com>
2025-03-07 13:58:23 +0900

    Update data.js

--- data.js
+++ data.js
@@ -1,8 +1,3 @@
 const employees = [
-    { name: "김철수", id: "10001", email: "chulsoo@example.com" },
-    { name: "이영희", id: "10002", email: "younghee@example.com" },
-    { name: "박민수", id: "10003", email: "minsoo@example.com" },
-    { name: "최지은", id: "10004", email: "jieun@example.com" },
-    { name: "홍길동", id: "10005", email: "gildong@example.com" },
-    { name: "박기동", id: "15410", email: "gidongg@example.com" }    
+    { Cname: "DAEBO MAGNETIC CO.,LTD.", Addr: "25_13, Hyundaikiaro, Paltanmyeon, Hwaseongsi, Gyeonggido, Republic of Korea", AppF: "", Ccode: "082-03-026-01424", Std: "Standard Certificate of Compliance", Cstat: "Valid", Cdate: "04-12-2029" }
 ];
