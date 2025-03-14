73064624c91a715c96427e04bcf3cde491076d23
0CHOPA0 <67321394+0CHOPA0@users.noreply.github.com>
2025-03-14 18:33:06 +0900

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,3 +1,4 @@
 const CertificateData = [
-    { Cname: "DAEBO MAGNETIC CO.,LTD.", Addr: "25_13, Hyundaikiaro, Paltanmyeon, Hwaseongsi, Gyeonggido, Republic of Korea", AppF: "", Ccode: "082-03-026-01424", Std: "Standard Certificate of Compliance", Cstat: "Valid", Cdate: "04-12-2029" }
+    { Cname: "DAEBO MAGNETIC CO.,LTD.", Addr: "25_13, Hyundaikiaro, Paltanmyeon, Hwaseongsi, Gyeonggido, Republic of Korea", AppF: "", Ccode: "082-03-026-01424", Std: "Standard Certificate of Compliance", Cstat: "Valid", Cdate: "04-12-2029" },
+    { Cname: "Test CO.,LTD.", Addr: "111-111, Yeoui-daero, Yeongdeungpo-gu, Seoul, Republic of Korea", AppF: "", Ccode: "111-11-111-11111", Std: "Test", Cstat: "Invalid", Cdate: "04-12-2022" }
 ];
